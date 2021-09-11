a0d5bd5f70f374a61c24738b5198511595b034db
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-09-11 17:23:35 +0300

    Добавляет модальное окно в секции контакты

--- src/index.js
+++ src/index.js
@@ -14,4 +14,20 @@ import './sass/main.scss';
       document.body.classList.toggle("modal-open");
       refs.modal.classList.toggle('is-hidden');
     }
+})();
+  
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-contacts]'),
+      closeModalBtn: document.querySelector('[data-contacts-close]'),
+      modal: document.querySelector('[data-modal-contacts]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden');
+    }
   })();
