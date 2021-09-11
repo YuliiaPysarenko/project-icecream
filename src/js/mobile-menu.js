bbf67d6fa4acc9b0308de98ea28d47767cbdb575
katerina2018 <tvorchestvo15@gmail.com>
2021-09-11 15:42:22 +0300

    change hero/bg and mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const closeMenuBtn = document.querySelectorAll('.js-close-menu');
 
     const toggleMenu = () => {
         const isMenuOpen =
@@ -9,17 +9,22 @@
         openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
         mobileMenu.classList.toggle('is-open');
 
-        const scrollLockMethod = !isMenuOpen ?
-            'disableBodyScroll' :
-            'enableBodyScroll';
-        bodyScrollLock[scrollLockMethod](document.body);
+        // const scrollLockMethod = !isMenuOpen ?
+        //     'disableBodyScroll' :
+        //     'enableBodyScroll';
+        // bodyScrollLock[scrollLockMethod](document.body);
     };
 
     openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
 
-    // Закрываем мобильное меню на более широких экранах
-    // в случае изменения ориентации устройства.
+
+    closeMenuBtn.forEach(function(item) {
+            item.addEventListener('click', toggleMenu);
+        })
+        // closeMenuBtn.addEventListener('click', toggleMenu);
+        // console.log(closeMenuBtn)
+        // Закрываем мобильное меню на более широких экранах
+        // в случае изменения ориентации устройства.
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
         if (!e.matches) return;
         mobileMenu.classList.remove('is-open');
