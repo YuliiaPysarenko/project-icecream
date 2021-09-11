b7b20b0eed113557b5363be1d44cfc49454c9645
Yuliia Pysarenko <yuliapysarenko@gmail.com>
2021-09-11 22:16:39 +0300

    Додає модальне вікно для секції about

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ import './sass/main.scss';
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-about]'),
+      closeModalBtn: document.querySelector('[data-about-close]'),
+      modal: document.querySelector('[data-modal-about]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
