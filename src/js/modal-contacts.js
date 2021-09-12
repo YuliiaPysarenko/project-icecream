9731657b73f5e5dc326ab27233dae2b6f62a71ea
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-09-12 11:31:22 +0300

    подключает скрипты

---
+++ src/js/modal-contacts.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-contacts]'),
+      closeModalBtn: document.querySelector('[data-contacts-close]'),
+      modal: document.querySelector('[data-modal-contacts]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+
+
+      document.body.classList.toggle("modal-open");
+
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
