1d8fe3d71f7d1fff9a5edbef58d1321317d0f490
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-09-12 17:26:05 +0300

    добавляет модальное окно для заказа франшизы

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -13,3 +13,19 @@
       refs.modal.classList.toggle('is-hidden');
     }
 })();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-submit-franchise]'),
+      closeModalBtn: document.querySelector('[data-submit-franchise-close]'),
+      modal: document.querySelector('[data-modal-submit-franchise]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+     function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden');
+    }
+})();
